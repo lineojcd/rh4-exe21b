1e93e0a839a9f126726cd3ad13f22eb24d0733b7
lineojcd <45730495+lineojcd@users.noreply.github.com>
2020-11-03 17:34:57 +0100

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+#rosrun my_package my_node_subscriber.py
+roslaunch my_package multiple_nodes.launch
